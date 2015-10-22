c0afe006993833fc5982d9e8a369b44bbdda7090
Michael Akinde <michaeloa@met.no>
2015-10-22 13:47:41 +0200

    New data set

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hackgp
+
+  sudo apt-get install git
+  git clone https://github.com/michaeloa/hackgp.git
+  hackgp/bootstrap.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo apt-get install git
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk
-git clone https://github.com/michaeloa/hackgp.git
